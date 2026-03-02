cf09bcf74f930e162e1733171d9914d927c5d14d
Anonimous44 <167201862+Anonimous44@users.noreply.github.com>
2026-03-02 12:49:11 -0500

    Modifier la méthode manger() du PoissonClown
    Remplacement du message irréaliste par une alimentation correspondant au régime naturel du poisson-clown (plancton, crustacés, algues).

--- src/HaroldDominguez/aquarium/PoissonClown.java
+++ src/HaroldDominguez/aquarium/PoissonClown.java
@@ -8,6 +8,6 @@ public class PoissonClown extends Poisson {
 
     @Override
     public void manger() {
-        System.out.println(nom + " mange des frites au ketchup");
+        System.out.println(nom + " mange du plancton et de petits crustacés près de son anémone.");
     }
 }
